18fcbfa3605590f4ff4fc716f3f74cb2ad9bfa9f
xyznasty <75307250+xyznasty@users.noreply.github.com>
2021-01-23 23:36:22 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -64,6 +64,6 @@ def handle_message(event):
 
 # ポート番号の設定
 if __name__ == "__main__":
-#    app.run()
-    port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run()
+    #port = int(os.getenv("PORT", 5000))
+    #app.run(host="0.0.0.0", port=port)
