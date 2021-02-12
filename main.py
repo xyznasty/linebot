b9f2eec7192788a15ae020739763bd5250532036
zxynasty <xyznasty@gmail.com>
2021-02-12 17:32:58 +0900

    test

--- main.py
+++ main.py
@@ -34,10 +34,10 @@ def callback():
     app.logger.info("Request body: " + body)
 
     # handle webhook body
-　# 署名を検証し、問題なければhandleに定義されている関数を呼び出す。
+　# 署名を検証し、問題なければhandleに定義されている関数を呼び出す
     try:
         handler.handle(body, signature)
-　# 署名検証で失敗した場合、例外を出す。
+　# 署名検証で失敗した場合、例外を出す
     except InvalidSignatureError:
         abort(400)
 　# handleの処理を終えればOK
